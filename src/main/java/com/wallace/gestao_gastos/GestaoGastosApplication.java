3c52c9deea81cee8d519f91e8369623f18ab9269
walldev301089 <wallace89.dev@gmail.com>
2026-04-29 18:49:51 -0300

    primeiro commit

---
+++ src/main/java/com/wallace/gestao_gastos/GestaoGastosApplication.java
@@ -0,0 +1,13 @@
+package com.wallace.gestao_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoGastosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wallace/gestao_gastos/GestaoGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.gestao_gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
